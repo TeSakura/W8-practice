6bd6f74b4691cced888eced90115e68b6e2da5f5
TeSakura <149454188+TeSakura@users.noreply.github.com>
2025-11-27 21:48:27 +0700

    Rename 'value' to 'data' in TileData class
    Renamed 'value' to 'data' in TileData class for consistency.

--- W8-Practice/EXERCISE-3/model/profile_tile_model.dart
+++ W8-Practice/EXERCISE-3/model/profile_tile_model.dart
@@ -9,15 +9,15 @@ class ProfileData {
   ProfileData({
     required this.name,
     required this.position,
-    required this.tiles,
     required this.avatarUrl,
+    required this.tiles,
   });
 }
 
 class TileData {
   final IconData icon;
   final String title;
-  final String value;
+  final String data; // Changed from 'value' to 'data' to match requirement
 
-  TileData({required this.icon, required this.title, required this.value});
+  TileData({required this.icon, required this.title, required this.data});
 }
