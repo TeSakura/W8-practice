230dc94a048df94da04721754654467cfaa80adf
TeSakura <149454188+TeSakura@users.noreply.github.com>
2025-11-27 21:13:48 +0700

    Update main.dart

--- W8-Practice/EXERCISE-4/main.dart
+++ W8-Practice/EXERCISE-4/main.dart
@@ -1,71 +1,106 @@
 import 'package:flutter/material.dart';
+import 'jokes.dart';
 
 Color appColor = Colors.green[300] as Color;
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: appColor,
-          title: const Text("Favorite Jokes"),
-        ),
-        body: const Column(
-          children: [FavoriteCard(), FavoriteCard(), FavoriteCard()],
-        ),
+void main() => runApp(
+  MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: appColor,
+        title: const Text("Favorite Jokes"),
       ),
-    ));
+      body: JokeList(jokes: jokes),
+    ),
+  ),
+);
 
-class FavoriteCard extends StatefulWidget {
-  const FavoriteCard({
-    super.key,
-  });
+class JokeList extends StatefulWidget {
+  final List<Joke> jokes;
+
+  const JokeList({super.key, required this.jokes});
 
   @override
-  State<FavoriteCard> createState() => _FavoriteCardState();
+  State<JokeList> createState() => _JokeListState();
 }
 
-class _FavoriteCardState extends State<FavoriteCard> {
-  bool _isFavorite = false;
+class _JokeListState extends State<JokeList> {
+  int? _favoriteIndex;
 
-  void onFavoriteClick() {
+  void _toggleFavorite(int index) {
     setState(() {
-      _isFavorite = !_isFavorite;
+      if (_favoriteIndex == index) {
+        // Unfavorite if clicking the same joke
+        _favoriteIndex = null;
+      } else {
+        // Set new favorite
+        _favoriteIndex = index;
+      }
     });
   }
- 
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: widget.jokes.length,
+      itemBuilder: (context, index) {
+        return FavoriteCard(
+          joke: widget.jokes[index],
+          isFavorite: _favoriteIndex == index,
+          onFavoriteClick: () => _toggleFavorite(index),
+        );
+      },
+    );
+  }
+}
+
+class FavoriteCard extends StatelessWidget {
+  final Joke joke;
+  final bool isFavorite;
+  final VoidCallback onFavoriteClick;
+
+  const FavoriteCard({
+    super.key,
+    required this.joke,
+    required this.isFavorite,
+    required this.onFavoriteClick,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
-        border: Border(
-          bottom: BorderSide(width: .5, color: Colors.grey),
-        ),
+        border: Border(bottom: BorderSide(width: .5, color: Colors.grey)),
       ),
       padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-           Expanded(
+          Expanded(
             flex: 7,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  'title',
+                  joke.title,
                   style: TextStyle(
-                      color: appColor, fontWeight: FontWeight.w800),
+                    color: appColor,
+                    fontWeight: FontWeight.w800,
+                  ),
                 ),
                 const SizedBox(height: 10.0),
-                const Text('description')
+                Text(joke.description),
               ],
             ),
           ),
           IconButton(
-              onPressed: onFavoriteClick,
-              icon: Icon(
-                _isFavorite ? Icons.favorite : Icons.favorite_border,
-                color: _isFavorite ? Colors.red : Colors.grey,
-              ))
+            onPressed: onFavoriteClick,
+            icon: Icon(
+              isFavorite ? Icons.favorite : Icons.favorite_border,
+              color: isFavorite ? Colors.red : Colors.grey,
+            ),
+          ),
         ],
       ),
     );
